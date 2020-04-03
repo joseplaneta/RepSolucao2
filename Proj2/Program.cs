60a9f84520c1a1e954b77c31118d0e6b040b7f9f
Zeca <jose.planeta@yahoo.com.br>
2020-04-03 19:26:25 -0300

    Segundo commit da solucao2

--- Proj2/Program.cs
+++ Proj2/Program.cs
@@ -4,6 +4,7 @@ namespace Proj2 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World_2!");
         }
     }
 }
